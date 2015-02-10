8cd2f88039a58c8c0938e76f9189cfd93f768bb7
Justin Love <git@JustinLove.name>
2015-02-09 22:14:05 -0600

    layer selection

--- ui/mods/strategic_filters/live_game.js
+++ ui/mods/strategic_filters/live_game.js
@@ -8,9 +8,28 @@
   }
   ping()
   window.iconOff = function() {
-    atlasMessage.message('icon_atlas', 'toggle_icons', {off: ['commander']})
+    atlasMessage.message('icon_atlas', 'toggle_icons', {off: orbitalLayer})
   }
   window.iconOn = function() {
-    atlasMessage.message('icon_atlas', 'toggle_icons', {on: ['commander']})
+    atlasMessage.message('icon_atlas', 'toggle_icons', {on: orbitalLayer})
   }
+
+  var groundLayer = []
+  var airLayer = []
+  var orbitalLayer = []
+
+  console.log('strategic filter')
+  bif.registerBIFReadyCallback(function() {
+    groundLayer = bif.getFilteredUnitIDs('Land | Naval')
+    airLayer = bif.getFilteredUnitIDs('Air | AirDefense').filter(function(id) {
+      if (id.match('factory')) {
+        groundLayer.push(id)
+        return false
+      } else {
+        return true
+      }
+    })
+    orbitalLayer = bif.getFilteredUnitIDs('Orbital | OrbitalDefense')
+  })
+
 })()
